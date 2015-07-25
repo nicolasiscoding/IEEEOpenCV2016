0e57d01fcd90c8d3b8e13fea10dcf06fab4e3308
nicolasiscoding <nicolascoding@gmail.com>
2015-07-24 20:49:48 -0400

    Cleaning house to make room for Gabriel's code.

--- README.md
+++
@@ -1,4 +0,0 @@
-# objectRecognition
-IEEE hardware team 2016
-
-This is the repo for creating object recogition using ROS and openCV for the IEEE hardware team of 2015
--- objectRecognition/build/objectID/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nicolas/objectRecognition_ws/src/objectID
-# Build directory: /home/nicolas/objectRecognition_ws/build/objectID
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
